06a66ef35355f45b64c879390301cbb0135f60e9
widamudrikah <mudrikahwida@gmail.com>
2024-10-07 19:04:17 +0700

    dongengku submission

---
+++ app/src/main/java/com/wida/myflexiblefragment/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.wida.myflexiblefragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+//    Di sini kita bisa gunakan untuk inisialisasi view, dan juga mengatur action-nya (set listener).
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnCategory: Button = view.findViewById(R.id.btn_category)
+        btnCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v?.id == R.id.btn_category) {
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)    //untuk kembali ke tampilan sebelumnya
+                commit()
+            }
+        }
+    }
+}
